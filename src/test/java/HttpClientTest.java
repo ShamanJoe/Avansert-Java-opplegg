ef9768b3c51b6d2a932fe9ff0fb36cbe8e4b3e00
ShamanJoe <77103085+ShamanJoe@users.noreply.github.com>
2021-09-14 11:05:14 +0200

    Gitignore. Lagd første test som passerer

---
+++ src/test/java/HttpClientTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+    @Test
+    void shouldReturnStatusCode(){
+        assertEquals(200, 100+100);
+    }
+}
