95d900550b30176b96ed5ed893b7b341cf30ee26
ShamanJoe <77103085+ShamanJoe@users.noreply.github.com>
2021-09-14 18:42:19 +0200

    shouldReturnStatusCode (passing) med refaktorert kode. shouldReadResponseHeaders (failing)

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -1,9 +1,40 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class HttpClient {
 
-    public HttpClient(String host, int port, String requestTarget) {
+    private final int statuscode;
+
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port);
+
+        String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Host:" + host + "\r\n" +
+                "Connection: close\r\n" +
+                "\r\n";
+
+        socket.getOutputStream().write(request.getBytes());
+
+        String[]statusLine = readLine(socket).split(" ");
+        this.statuscode = Integer.parseInt(statusLine[1]);
+
+    }
+
+    private String readLine(Socket socket) throws IOException {
+        StringBuilder result = new StringBuilder();
+        int c;
+        while ((c = socket.getInputStream().read()) != '\r') {
+          result.append((char) c);
+        }
+
+        return result.toString();
     }
 
     public int getStatusCode() {
-        return 200;
+        return statuscode;
+    }
+
+    public String getHeader(String headerName) {
+        return null;
     }
 }
--- src/test/java/HttpClientTest.java
+++ src/test/java/HttpClientTest.java
@@ -1,21 +1,24 @@
+
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
     @Test
-    void shouldDoSomething(){
-        assertEquals(200, 100+100);
-    }
-    @Test
-    void shouldReturnStatusCode(){
-        assertEquals(200,
-                new HttpClient("httpbin.org", 80, "/html")
-                        .getStatusCode());
-        assertEquals(404,
-                new HttpClient("httpbin.org", 80, "/no-such-page")
-                        .getStatusCode());
+    void shouldReturnStatusCode() throws IOException {
+        assertEquals(200, new HttpClient("httpbin.org", 80, "/html").getStatusCode());
 
+        assertEquals(404, new HttpClient("httpbin.org", 80, "/this-page-does-not-exist").getStatusCode());
+
+        // Denne testen vil feile : assertEquals(401, new HttpClient("httpbin.org", 80, "/status/403").getStatusCode());
+    }
+
+    @Test
+    void shouldReadResponseHeaders() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        assertEquals("text/html; charset=utf-8", client.getHeader("Content-Type"));
     }
 
 }
